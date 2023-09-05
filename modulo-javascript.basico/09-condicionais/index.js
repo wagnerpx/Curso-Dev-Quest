63fbc2edc390f087d9386a7d8d229137f3923fe3
wagnerpx <impussivo@gmail.com>
2023-09-04 22:48:41 -0300

    Atualizar

---
+++ modulo-javascript.basico/09-condicionais/index.js
@@ -0,0 +1,15 @@
+let nomeFilme = 'Vingadores'
+
+// if(nomeFilme === 'Vingadores'){
+//     console.log('É o filme dos Vingadores')
+// }else if(nomeFilme === 'KingKong'){
+//     console.log('É o filme KingKong')
+// }else if(nomeFilme === 'Capitão América'){
+//     console.log('É o filme Capitão América')
+// }else{
+//     console.log('É outro filme')
+// }
+
+true ? console.log('condição verdadeira') : console.log('condição falsa')
+
+nomeFilme === 'KingKong' ? console.log('É o filme dos Vingadores') : console.log('É outro filme')
---
+++ modulo-javascript.basico/10-condicionais-switch/index.js
@@ -0,0 +1,40 @@
+//switch(parametro){
+//    case valor1:
+//        <bloco de declaração>
+//        break
+//    case valo2:
+//        <bloco de declaração>
+//        break
+//}
+
+// let nomeFilme = 'Mulher Gato'
+
+// switch(nomeFilme){
+//     case 'Vingadores':
+//         console.log('É o filme dos Vingadorse')
+//         break
+//     case 'Batman vs Superman':
+//         console.log('É o filme do Batman vs Superman')
+//         break
+//     case 'Senhor dos Anéis':
+//         console.log('É o Senhor do Anéis')
+//         break
+//     default:
+//         console.log('É outro filme')
+//         break
+// }
+
+let avaliacao = 1
+
+switch(avaliacao){
+    case 1:
+    case 2:
+        console.log('Filme ruim')
+        break
+    case 3:
+    case 4:
+        console.log('Filme mediano')
+        break
+    case 5:
+    case 6:
+}
