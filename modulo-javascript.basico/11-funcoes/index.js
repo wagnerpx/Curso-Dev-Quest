5fa10c8677737762dcab070829bc3d2414858e0b
wagnerpx <impussivo@gmail.com>
2023-09-05 16:45:33 -0300

    Atualizar exercício java básico

---
+++ modulo-javascript.basico/11-funcoes/index.js
@@ -0,0 +1,31 @@
+// function nomeDaFuncao(){
+//     <bloco de execucao>
+// }
+// nomeDaFuncao()
+
+
+
+// function nomeDaFuncao(parametro1, parametro2){
+//     <bloco de execucao>
+// }
+
+// nomeDaFuncao()
+
+
+
+function incentivarQuester(nomeQuester = 'Quester'){
+    alert('Muito bem '+ nomeQuester + ' vc chegou ao js, continue assim!')
+}
+
+incentivarQuester()
+
+
+
+// function soma(numero1, numero2){
+//     return numero1 + numero2
+// }
+// let resultadoDaSoma = soma(1,3)
+// console.log(resultadoDaSoma)
+
+// resultadoDaSoma = soma(resultadoDaSoma, 10)
+// console.log(resultadoDaSoma)    
