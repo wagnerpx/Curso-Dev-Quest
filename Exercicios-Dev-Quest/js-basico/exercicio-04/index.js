1b552018d8a5fb4849b03756a88da313abffe16c
wagnerpx <impussivo@gmail.com>
2023-09-06 23:21:29 -0300

    Resolução do exercício 4 de javascript

---
+++ Exercicios-Dev-Quest/js-basico/exercicio-04/index.js
@@ -0,0 +1,3 @@
+for(let b = 0; b <= 10; b++){
+    console.log('"O número atual é o ' + b + '"')
+}
--- modulo-javascript.basico/07-laco-for/index.js
+++ modulo-javascript.basico/07-laco-for/index.js
@@ -1,6 +1,7 @@
 //for(inicializador; condicao; incremento)
 // decleracao
 
-for(let i = 0; i < 5; i++){
-    console.log(i)
+for(let i = 0; i < 11; i++){
+    console.log('"O número atual é o ' + i + '"')
+    
 }
