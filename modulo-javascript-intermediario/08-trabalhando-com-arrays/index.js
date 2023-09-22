4fa9f6664ecc3cd69259de8f339f0c077471e555
wagnerpx <impussivo@gmail.com>
2023-09-21 21:26:14 -0300

    Atualização dos exercícios

---
+++ modulo-javascript-intermediario/08-trabalhando-com-arrays/index.js
@@ -0,0 +1,47 @@
+let frutas = ['banana', 'maçã', 'laranja', 'pêra']
+
+frutas.push('manga')
+
+console.log(frutas)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let frutasTamanho = frutas.length
+
+// for(let i = 0; i < frutas.length; i++){
+//     console.log('Fruta: ' + frutas[i])
+// }
+
+// frutas.forEach(function(item, indice, array){
+//     console.log('Fruta: ' + frutas[indice])
+// })
+
+/*simplificado */
+
+// frutas.forEach(function(item){
+//     console.log('Fruta: ' + item)
+// })
+
