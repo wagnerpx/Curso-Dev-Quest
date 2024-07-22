6f77795b82622651d13569db34a42e002382a294
wagnerpx <impussivo@gmail.com>
2024-07-22 16:20:46 -0300

    Atualizar exercício

--- modulo-javascript-avancado/06-operador-spread/index.js
+++ modulo-javascript-avancado/06-operador-spread/index.js
@@ -1,4 +1,12 @@
-const pais = ['Pedro', 'Paulo', 'Caninde']
-const filhos = ['Manoel', 'Jocasta', 'Manuella']
+const veteranos = ['Pedro', 'Paulo', 'Caninde']
+const calouros = ['Manoel', 'Jocasta', 'Manuella']
 
-console.log(...pais, ...filhos);
+//CONCATENAR 2 OU MAIS ARREIS
+
+//console.log(...veteranos, ...calouros);
+
+const pessoas = {nome: 'Roberto', idade: 41, cidade: 'Garulhos'}
+
+const objetoClonado = {...pessoas, ...veteranos}
+console.log(objetoClonado);
+console.log(pessoas);
