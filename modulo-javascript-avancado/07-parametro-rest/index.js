ee90ba9b093cc8d30193374fc0490d9308109b20
wagnerpx <impussivo@gmail.com>
2024-07-30 12:15:41 -0300

    Atualização

---
+++ modulo-javascript-avancado/07-parametro-rest/index.js
@@ -0,0 +1,5 @@
+function incentivarQuester(mensagem, ...nomesQuesters){
+    nomesQuesters.map(nomeQuester => console.log(`${mensagem} ${nomeQuester}`))
+}
+
+incentivarQuester('Parabens por ter chegado ao modulo de javaScript avançado,', 'Ismael', 'Rafael', 'Samuel')
